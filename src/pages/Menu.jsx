2a55a435fe5e7ff80f9b7d2397f24fadac340835
Attaullah-Samejo <attaullahsamejo684@gmail.com>
2026-09-08 13:17:09 -0700

    fix product data

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -7,7 +7,7 @@ import {
   ShoppingBag,
   Star,
 } from "lucide-react";
-import menuData from "../data/MenuData";
+import menuData from "../data/ProductData";
 import { useCart } from "../context/CartContext";
 
 function Menu() {
